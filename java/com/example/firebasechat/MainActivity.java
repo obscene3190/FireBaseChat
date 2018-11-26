3593587e5360ea56b36e95cab0fd677435cf30a4
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-11-26 23:40:22 +0300

    добавлена часть пользователя для регистрации

--- java/com/example/firebasechat/MainActivity.java
+++ java/com/example/firebasechat/MainActivity.java
@@ -20,6 +20,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.concurrent.TimeUnit;
 
@@ -34,6 +39,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button Login;
     private Button Registration;
+
+    User current_user;
+
+    DatabaseReference Admen = FirebaseDatabase.getInstance().getReference("Admen");
+    DatabaseReference messages = FirebaseDatabase.getInstance().getReference("messages");
+    DatabaseReference pkeys = FirebaseDatabase.getInstance().getReference("Public_Key");
+    DatabaseReference test = FirebaseDatabase.getInstance().getReference("test");
+    DatabaseReference users = FirebaseDatabase.getInstance().getReference("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,9 +98,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if(task.isSuccessful())
                 {
                     Toast.makeText(MainActivity.this, "Регистрация успешна", Toast.LENGTH_SHORT).show();
+                    //...
+                    // Процесс регистрации для пользователя: он создает ключи, отправляет их и ждет
+                    /*
+                    current_user = new User(0);
+                    pkeys.child(mAuth.getInstance().getCurrentUser().getUid()).setValue(current_user.userPubKeyEncStr);
+                    Toast.makeText(MainActivity.this, "Ключ отправлен, ожидайте ответа Администратора", Toast.LENGTH_SHORT).show();
+                    Admen.child(mAuth.getInstance().getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            String[] result = new String[4];
+                            result[0] = dataSnapshot.child("adminPubKeyEnc").getValue(String.class);
+                            result[1] = dataSnapshot.child("cipherString1").getValue(String.class);
+                            result[2] = dataSnapshot.child("cipherString2").getValue(String.class);
+                            result[3] = dataSnapshot.child("encodedParams").getValue(String.class);
+                            if ((result[0] != null) && (result[1] != null) && (result[2] != null) && (result[3] != null)) {
+                                current_user.EncryptSession(result);
+                                users.child(mAuth.getInstance().getCurrentUser().getUid()).child("session1").setValue(current_user.sessionPair_[0]);
+                                users.child(mAuth.getInstance().getCurrentUser().getUid()).child("session2").setValue(current_user.sessionPair_[1]);
+                                Admen.removeValue();
+                                pkeys.removeValue();
+                                Toast.makeText(MainActivity.this, "Ключ получен", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                    */
+                    //...
                     Intent intent = new Intent(MainActivity.this,ChatRoom.class);
                     startActivity(intent);
-                    // надо бы сделать обмен ключами на этапе регистрации, чтобы пользователь не ждал 500 часов при заходе в чат...
                 }
                 else
                     Toast.makeText(MainActivity.this, "Регистрация провалена", Toast.LENGTH_SHORT).show();
